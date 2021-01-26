ccd7c718479fea7213149b11c71120a0b6ab7d44
Pranab Das <thepranab@live.in>
2021-01-26 17:38:07 +0800

    04 addition app

--- README.md
+++ README.md
@@ -1,17 +1,9 @@
-# React App component
+# React Add two numbers
   
 [Go back to master branch](https://github.com/pranabdas/react-learning/tree/master)
 
-Now we will separate the app component to a new file leaving our `index.js` 
-clean. 
+We will build an app to add two numbers. We will only change the `App.jsx` file
+for this lesson.
 
-Our `index.js` becomes:
-```js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
+Main app is here: [my-react-app/src/App.jsx](./my-react-app/src/App.jsx)
 
-ReactDOM.render(< App />, document.getElementById('root'));
-```
-
-We will have a new file `App.jsx` which will be our actual app. 
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
-
-const user = {
-    "firstName": "Pranab", 
-    "lastName" : "Das"
-}
+import React, { useState } from 'react';
 
 function App() {
+    const [input1, setInput1] = useState(0);
+    const [input2, setInput2] = useState(0);
+    const [total, setTotal] = useState(input1 + input2);
+
+    function add() {
+        setTotal(input1 + input2)
+    }
+
     return(
-      <>
-        <h1>React App</h1>
-        <p>Hello {user.firstName} {user.lastName}!</p>
+    <>
+        <h3>Adding two numbers</h3>
+        <input 
+            type="number"
+            value={input1}
+            onChange={e => setInput1(+e.target.value)}
+            placeholder="0"
+        />
+        <input 
+            type="number"
+            value={input2}
+            onChange={e => setInput2(+e.target.value)}
+            placeholder="0"
+        />
+        <button onClick={add}>Add</button>
+        <p>{total}</p>
       </>
     )
   }
