6df9bbf2f11b13968f556c863389766c0fa3b8ad
Pranab Das <thepranab@live.in>
2021-01-27 11:35:37 +0800

    06 props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
 const user = {
-    "firstName": "Pranab", 
-    "lastName" : "Das"
+  "firstName": "Pranab", 
+  "lastName" : "Das"
+}
+
+function Welcome(props) {
+  return(
+    <>
+    <h3>React Props (properties)</h3>
+    <p>Hello {props.user.firstName} {props.user.lastName}!!</p>
+  </>
+  );
 }
 
 function App() {
     return(
-      <>
-        <h1>React App</h1>
-        <p>Hello {user.firstName} {user.lastName}!</p>
-      </>
+      <Welcome user={user} />
     )
-  }
+}
 
 export default App
