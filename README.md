44e76f02801843b01d2797c787050dfb736d0524
Pranab Das <thepranab@live.in>
2021-01-27 17:59:30 +0800

    07-book-list

--- README.md
+++ README.md
@@ -1,17 +1,9 @@
-# React App component
-  
-[Go back to master branch](https://github.com/pranabdas/react-learning/tree/master)
+# List of best selling books
 
-Now we will separate the app component to a new file leaving our `index.js` 
-clean. 
+[Go back to main branch](https://github.com/pranabdas/react-learning/tree/main)
 
-Our `index.js` becomes:
-```js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
+In this app, we will create the list of best selling books from [Amazon](
+https://www.amazon.com/best-sellers-books-Amazon/zgbs/books).
 
-ReactDOM.render(< App />, document.getElementById('root'));
-```
+Main app is here: [my-react-app/src/App.jsx](./my-react-app/src/App.jsx)
 
-We will have a new file `App.jsx` which will be our actual app. 
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,52 @@
 import React from 'react';
 
-const user = {
-    "firstName": "Pranab", 
-    "lastName" : "Das"
+const bookList = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/91wRcPETM0L._AC_UL200_SR200,200_.jpg",
+    title: "Change Sings: A Children's Anthem", 
+    author: "Amanda Gorman"
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/7196WwHcD4L._AC_UL200_SR200,200_.jpg",
+    title: "The Hill We Climb and Other Poems", 
+    author: "Amanda Gorman"
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/91pR9wKJ3zL._AC_UL200_SR200,200_.jpg",
+    title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones", 
+    author: "James Clear"
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/71V7ISaf5FL._AC_UL200_SR200,200_.jpg",
+    title: "The Hill We Climb: An Inaugural Poem for the Country", 
+    author: "Amanda Gorman"
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/7184Pp9Y-oL._AC_UL200_SR200,200_.jpg",
+    title: "Keep Sharp: Build a Better Brain at Any Age", 
+    author: "Sanjay Gupta M.D."
+  }
+];
+
+const BookCard = (props) => {
+  return(
+    <article>
+      <img src={props.img} alt="" />
+      <h3>{props.title}</h3>
+      <h4>{props.author}</h4>
+      <hr/>
+    </article>
+  )
 }
 
 function App() {
     return(
       <>
-        <h1>React App</h1>
-        <p>Hello {user.firstName} {user.lastName}!</p>
+        {bookList.map((item, index) => {
+          return (
+            <BookCard {...item} key={index} />  // spread operator
+          );
+        })}
       </>
     )
   }
