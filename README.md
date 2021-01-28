8432a1a8e9fc8ef8b6903eb62aef4321c6b7d16e
Pranab Das <thepranab@live.in>
2021-01-28 16:55:38 +0800

    16 conditional rendering

--- README.md
+++ README.md
@@ -1,28 +1,6 @@
-# React useEffect
-  
-[Go back to master branch](https://github.com/pranabdas/react-learning/tree/master)
+# Conditional rendering
 
-Here we will use useEffect to update the browser title in our previous counter
-example. We will update the counter on the title everytime we click any button.
-
-`useEffect` runs on every re-render. If you want the `useEffect` to run only on
-the initial run, we can pass a second argument with empty array: 
-```js
-React.useEffect(() => {
-    document.title = `Current number (${value})`;
-  }, []);
-```
-
-This second argument can also be used to call useEffect only after change in 
-certain parameter, say, `value` in our case: 
-```js
-React.useEffect(() => {
-    document.title = `Current number (${value})`;
-  }, [value]);
-```
-
-We can have multiple `useEffect`, say one for the initial render, and another
-for when the `value` changes. 
+[Go back to main branch](https://github.com/pranabdas/react-learning/tree/main)
 
 Main app is here: [my-react-app/src/App.jsx](./my-react-app/src/App.jsx)
 
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
 
 function App() {
-
-  const [value, setValue] = React.useState(0);
+  const url = "https://api.github.com/users/pranabdas"; 
+  const [isLoading, setLoading] = React.useState(true);
+  const [isError, setError] = React.useState(false);
+  const [user, setUser] = React.useState('default user');
 
   React.useEffect(() => {
-    document.title = `Current number (${value})`;
-  });
+    fetch(url)
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status < 300) {
+          return resp.json()
+        }
+        else {
+          setLoading(false);
+          setError(true);
+          throw new Error(resp.statusText);
+        }
+      })
+      .then((user) => {
+        setUser(user.login)
+        setLoading(false)
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (isLoading) {
+    return <h3>Loading...</h3>
+  };
+
+  if (isError) {
+    return <h3>There was an error!</h3>
+  };
 
   return(
     <>
-      <h1>Counter</h1>
-      <h4>{value}</h4>
-      <button onClick={() => setValue(value+1)}>Increase</button>
-      <button onClick={() => setValue(0)}>Reset</button>
+      <h1>Conditional Rendering</h1>
+      <h4>{user}</h4>
     </>
   );
 }
