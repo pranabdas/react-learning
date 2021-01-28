0301c993b242284d27273bae355f2390970e9575
Pranab Das <thepranab@live.in>
2021-01-28 18:52:43 +0800

    18 react form

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
-# React useState
+# React form
   
 [Go back to master branch](https://github.com/pranabdas/react-learning/tree/master)
-
-Here we will use useState to change some elements of our app. We will click a
-button and change the heading.
 
 Main app is here: [my-react-app/src/App.jsx](./my-react-app/src/App.jsx)
 
--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,61 @@
-import React, { useState } from 'react';
+import React  from 'react';
 
 function App() {
 
-  const [title, setTitle] = useState("Hello React");
+  const [firstName, setFirstName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [people, setPeople] = React.useState([]);
 
-  const handleClick = () => {
-    (title === "Hello React") ? setTitle("Hello useState") : setTitle("Hello React")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email }
+      setPeople((people) => {
+        return [...people, person]
+      });
+      setFirstName('');
+      setEmail('');
+    } else {
+      console.log('Empty value');
+    }
   };
 
   return(
     <>
-      <h1>{title}</h1>
-      <button onClick={handleClick}>Change title</button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">Name: </label>
+          <input 
+          type="text" 
+          id="firstName" 
+          name="firstName" 
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="email">Email: </label>
+          <input 
+          type="text" 
+          id="email" 
+          name="email" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </div>
+        <button type="submit">Add person</button>
+      </form>
+      {
+        people.map((person) => {
+          const { id, firstName, email } = person;
+          return (<div key={id}>
+            <h4>{firstName}</h4>
+            <p>{email}</p>
+          </div>);
+        })
+      }
     </>
   );
-}
+};
 
 export default App
