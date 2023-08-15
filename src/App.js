4cc9c50208756aa14a1842817683754ba62fe1a5
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2023-08-15 15:34:10 +0000

    implementation of useState and add new note

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import data from "./Data";
 
 function App() {
+  const [notes, setNotes] = useState(data);
+  const [newNote, setNewNote] = useState("");
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const newNoteObject = {
+      content: newNote,
+      important: false,
+      id: notes.length + 1,
+    };
+    
+    setNotes(notes.concat(newNoteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
   return (
     <>
       <h1>My notes</h1>
       <ol>
-        {data.map((note) => (
+        {notes.map((note) => (
           <li key={note.id}>{note.content}</li>
         ))}
       </ol>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">Save</button>
+      </form>
     </>
   );
 }
